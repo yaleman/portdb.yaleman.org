74071a3a4d9d747d7dc5af2156be4d56dba3caf1
James Hodgkinson <james@terminaloutcomes.com>
2022-02-10 19:11:49 +1000

    updates

--- generatecontent.py
+++ generatecontent.py
@@ -2,11 +2,11 @@
 
 """ content generator """
 
-from multiprocessing import Pool
+import asyncio
 import os
 from pathlib import Path
 
-DATADIR = "data"
+DATADIR = "./data"
 OUTPUT_DIR = "docs"
 
 PROTOCOLS = [ 'tcp', 'udp' ]
@@ -22,10 +22,9 @@ url: /{protocol}/{port}
 [Home](/) - [{protocol}](/{protocol}/) - {port}
 """
 
-def do_content(input_data):
+async def do_content(protocol, port, show: bool):
     """ takes a protocol and port tuple and does the processing for that combination """
 
-    protocol, port = input_data
     portdir = f"{DATADIR}/{protocol}/{port}"
     portfile = f"{OUTPUT_DIR}/{protocol}/{port}.md"
     if os.path.isdir(portdir):
@@ -33,45 +32,55 @@ def do_content(input_data):
         # base template
         portdata = POST_TEMPLATE.format(**info)
         notes = ianadata = False
-        notesfile = Path("{portdir}/notes.md")
+        notesfile = Path(f"{portdir}/notes.md").resolve()
         if notesfile.exists():
             # notes file exists
-            portdata += "\n"+notesfile.read_bytes()
+            portdata += f"\n{notesfile.read_text(encoding='utf8')}"
+            if show:
+                print(f"Notes for {protocol}/{port}:\n{notesfile.read_text(encoding='utf8')}")
             notes = True
+        elif show:
+            print(f"Notes file {notesfile} not found")
         ianafile = Path(f"{portdir}/iana.md")
         if ianafile.exists():
             # iana data exists
-            portdata += "\n# IANA Data\n\n"+ianafile.read_bytes()
+            portdata += f"\n# IANA Data\n\n{ianafile.read_text(encoding='utf8')}"
             ianadata = True
         if True not in (notes, ianadata):
             # die if there's no notes and data, that's weird.
-            raise ValueError(f"No notes/ianadata for {protocol}/{port}")
+            print(f"No notes/ianadata for {protocol}/{port}")
+            return False
         # check if we need to write to disk
         writefile = False
         portfile_ref = Path(portfile)
         if portfile_ref.exists():
-            if portdata != portfile_ref.read_bytes():
+            if portdata != portfile_ref.read_text(encoding='utf8'):
                 writefile = True
         else:
             writefile = True
         if writefile:
-            portfile_ref.write_bytes(portdata)
+            portfile_ref.write_text(portdata, encoding="utf8")
 
+async def main():
+    """ main func """
+    for proto in PROTOCOLS:
+        proto_output_dir = Path(f"{OUTPUT_DIR}/{proto}")
+        protodatadir = Path(f"{DATADIR}/{proto}")
+        # die if can't find protocol data, probably something going horribly wrong
+        if not protodatadir.exists():
+            raise FileNotFoundError(f"Can't find protocol data dir for '{proto}'")
+        # did you nuke the output dir? let's create it.
+        if not proto_output_dir.exists():
+            #os.mkdir(proto_output_dir)
+            proto_output_dir.mkdir()
+            print("Created protocol directory")
+        print((f"Processing {proto}"))
+        # do all the ports!
 
-for proto in PROTOCOLS:
-    proto_output_dir = Path(f"{OUTPUT_DIR}/{proto}")
-    protodatadir = Path(f"{DATADIR}/{proto}")
-    # die if can't find protocol data, probably something going horribly wrong
-    if not protodatadir.exists():
-        raise FileNotFoundError(f"Can't find protocol data dir for '{proto}'")
-    # did you nuke the output dir? let's create it.
-    if not proto_output_dir.exists():
-        #os.mkdir(proto_output_dir)
-        proto_output_dir.mkdir()
-        print("Created protocol directory")
-    print((f"Processing {proto}"))
-    # do all the ports!
-    with Pool(processes=NUM_PROCESSES) as pool:
-        inputdata = [(proto, port) for port in os.listdir(protodatadir.as_posix())]
-        for i in pool.imap_unordered(do_content, inputdata):
-            pass
+        show = False
+        for port in os.listdir(protodatadir.as_posix()):
+            await do_content(proto, port, show=show)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
